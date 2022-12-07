0944e2c155d0128ae406ebbd6f453f46ab395c4f
NikVallin <businas@mail.ru>
2022-12-07 17:06:32 +0300

    переменные ДР

--- lesson 1.py
+++ lesson 1.py
@@ -1,1 +1,15 @@
-"dsggfdgfdgfdgdfg"
+10 # int
+2.5368 #float
+"str1" #str
+True #bool
+False #bool
+None #пустое значение
+per1 = "Hello"
+per2 = "Goodbye"
+per3 = 10
+per4 = 25
+per5 = 7.5297
+per6 = 100.002536
+per7 = False
+per8 = True
+per9 = None
